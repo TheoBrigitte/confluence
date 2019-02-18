1ecadbc5101806d5e1a7ac455d0eb658182a722f
Theo Brigitte <theo.brigitte@gmail.com>
2019-02-18 19:05:08 +0100

    add CORS header

--- confluence/handler.go
+++ confluence/handler.go
@@ -16,5 +16,6 @@ type Handler struct {
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	r = r.WithContext(context.WithValue(r.Context(), handlerContextKey, h))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	mux.ServeHTTP(w, r)
 }
