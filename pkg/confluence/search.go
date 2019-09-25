cec1331132cc7309c856cee5f9cc1e335d7cdedc
Theo Brigitte <theo.brigitte@gmail.com>
2019-09-25 15:30:17 +0200

    torrent: query 2 provider in parallel

--- pkg/confluence/search.go
+++ pkg/confluence/search.go
@@ -6,18 +6,63 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/TheoBrigitte/confluence/pkg/cpasbien"
+	"github.com/TheoBrigitte/confluence/pkg/movie"
 	"github.com/TheoBrigitte/confluence/pkg/yify"
+	"golang.org/x/sync/errgroup"
 )
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("searchHandler")
 	q := r.URL.Query()
+	query := q.Get("query")
 
-	c := yify.New()
-	movies, err := c.SearchMoviesWithBestTorrent(q.Get("query"))
-	if err != nil {
-		http.Error(w, fmt.Sprintf("search failed: %v", err.Error()), http.StatusNotFound)
+	var m = make(chan []movie.MovieTorrent)
+	var g errgroup.Group
+
+	// yify
+	{
+		g.Go(func() error {
+			var movies []movie.MovieTorrent
+			var err error
+			defer func() { m <- movies }()
+			y := yify.New()
+			movies, err = y.SearchMoviesWithBestTorrent(query)
+			if err != nil {
+				return fmt.Errorf("yify search failed: %v", err.Error())
+			}
+
+			return nil
+		})
+	}
+
+	// cpasbien
+	{
+		g.Go(func() error {
+			var movies []movie.MovieTorrent
+			var err error
+			defer func() { m <- movies }()
+			c, err := cpasbien.New(cpasbien.Config{})
+			if err != nil {
+				return fmt.Errorf("cpasbien init failed: %v", err.Error())
+			}
+			movies, err = c.Search(query)
+			if err != nil {
+				return fmt.Errorf("cpasbien search failed: %v", err.Error())
+			}
+
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
+	}
+
+	var movies []movie.MovieTorrent
+	for i := 0; i < 2; i++ {
+		movies = append(movies, <-m...)
 	}
 
 	json.NewEncoder(w).Encode(movies)
