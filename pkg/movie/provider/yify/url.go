c44f00b35c79a6011f33705fbf9ec909d138316a
Theo Brigitte <theo.brigitte@gmail.com>
2020-04-10 03:53:37 +0200

    yifi: domain yst.am

--- pkg/movie/provider/yify/url.go
+++ pkg/movie/provider/yify/url.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	baseURL, _ = url.Parse("https://yts.ai/api/v2/")
+	baseURL, _ = url.Parse("https://yst.am/api/v2/")
 
 	//https://yts.am/api/v2/list_movies.json?query_term=test
 	searchURL, _   = url.Parse("list_movies.json")
