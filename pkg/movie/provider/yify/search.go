ce079823e45191a99ffd531f72f44fe06155ee84
Theo Brigitte <theo.brigitte@gmail.com>
2020-05-11 18:43:02 +0200

    yify: do not export searchMovies

--- pkg/movie/provider/yify/search.go
+++ pkg/movie/provider/yify/search.go
@@ -2,9 +2,25 @@ package yify
 
 import (
 	"net/http"
+
+	"github.com/TheoBrigitte/confluence/pkg/movie"
 )
 
-func (c client) SearchMovies(query string) (*searchResponse, error) {
+func (c *client) Search(query string) ([]movie.MovieTorrent, error) {
+	res, err := c.searchMovies(query)
+	if err != nil {
+		return nil, err
+	}
+
+	movies := []movie.MovieTorrent{}
+	for _, m := range res.Data.Movies {
+		movies = append(movies, m.ToMovieTorrent())
+	}
+
+	return movies, nil
+}
+
+func (c client) searchMovies(query string) (*searchResponse, error) {
 	u := searchURL
 	q := u.Query()
 	q.Set(searchQueryKey, query)
