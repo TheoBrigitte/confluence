140ab46c8b520e1a138381765a4c3981b7b568d3
Theo Brigitte <theo.brigitte@gmail.com>
2020-05-11 19:27:30 +0200

    yify: update url with new query parameters

--- pkg/movie/provider/yify/url.go
+++ pkg/movie/provider/yify/url.go
@@ -7,7 +7,10 @@ import (
 var (
 	baseURL, _ = url.Parse("https://yst.am/api/v2/")
 
-	//https://yts.am/api/v2/list_movies.json?query_term=test
+	// Search query example:
+	// https://yts.am/api/v2/list_movies.json?query_term=test
 	searchURL, _   = url.Parse("list_movies.json")
 	searchQueryKey = "query_term"
+	searchLimitKey = "limit"
+	searchSortKey  = "sort_by"
 )
